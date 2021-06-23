f3d75a9dee674fc5c54818380a00985152196ecb
Martin Nanchev <mart.nanchev@gmail.com>
2021-06-23 19:45:56 +0300

    [feature/app] - add all files

--- iac/bin/iac.ts
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-import "source-map-support/register";
-import * as cdk from "@aws-cdk/core";
-import { MlOpsPipelineStack } from "../lib/ml-ops-pipeline-stack";
-const app = new cdk.App();
-new MlOpsPipelineStack(app, "MlOpsPipeline");
-
